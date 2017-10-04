c6534774a49f9759af1009d58ad9fd9d61f3a074
Mariusz Kosiór <mariusz.kkk@poczta.fm>
2017-10-04 23:45:34 +0200

    commit3

--- modules/formatTime.js
+++ modules/formatTime.js
@@ -6,7 +6,7 @@ function formatTime(secs){
  
     var divisor_for_seconds = divisor_for_minutes % 60;
     var seconds = Math.ceil(divisor_for_seconds);
-    console.log("Uptime " + hours + " godz.", minutes + " min.", seconds + " sec."); 
+    console.log("Uptime: " + hours + " godz.", minutes + " min.", seconds + " sec."); 
 }
 
 exports.print = formatTime;
